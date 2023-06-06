8663377ab19d766ad259e1773799f1439101e04b
Berylogutu <berylogutu43@gmail.com>
2023-06-06 17:56:10 +0300

    calculator app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import {styles} from './style.css'
 
-function App() {
+export default function App() {
+
+  const [result, setResult] = useState('')
+  
+
+  function handleClick(e) {
+    setResult(result.concat(e.target.name))
+  }
+ 
+  function clear() {
+    setResult('')
+  }
+
+  function backspace() {
+    setResult(result.slice(0, -1))
+  }
+
+  function handleResult() {
+
+    try {
+      setResult(eval(result).toString())
+
+    } catch (error) {
+      setResult('Error')
+    }
+    
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='container'>
+      <form className='form'>
+        <input 
+        type='text' 
+        placeholder='0'
+        value={result}
+        />
+      </form>
 
-export default App;
+      <div className='keypad'>
+        <button onClick={clear} name='clear'  className='clear key'>Clear</button>
+        <button onClick={backspace} name='backspace' className='backspace key'>C</button>
+        <button onClick={handleClick} name='/' className='key'>&divide;</button>
+        <button onClick={handleClick} name='7'>7</button>
+        <button onClick={handleClick} name='8'>8</button>
+        <button onClick={handleClick} name='9'>9</button>
+        <button onClick={handleClick} name='*' className='key'>&times;</button>
+        <button onClick={handleClick} name='4'>4</button>
+        <button onClick={handleClick} name='5'>5</button>
+        <button onClick={handleClick} name='6'>6</button>
+        <button onClick={handleClick} name='-' className='key'>&ndash;</button>
+        <button onClick={handleClick} name='1'>1</button>
+        <button onClick={handleClick} name='2'>2</button>
+        <button onClick={handleClick} name='3'>3</button>
+        <button onClick={handleClick} name='+' className='key'>+</button>
+        <button onClick={handleClick} name='0'>0</button>
+        <button onClick={handleClick} name='.'>.</button>
+        <button onClick={handleResult} name='=' className='equal key'>=</button>
+      </div>
+
+    </div>
+
+
+  )
+}
